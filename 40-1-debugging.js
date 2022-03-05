d2715386fa6c862ea07b04d8cf3994a2c5aaa97c
hasan-mohammad-bd <hasan.md.east@gmail.com>
2022-03-05 17:10:49 +0600

    first commit

---
+++ 40-1-debugging.js
@@ -0,0 +1,74 @@
+//40-1 Module overview, Dev tool overview, settings, shortcut keys
+//to be developer
+//******************************* */
+//better ability of coding.
+//understand other code.
+//debugging
+
+//throttling: different speed of internet to check.
+//find shortcut from setting.
+
+
+//40-2 Element tab, HTML event listener, accessibility list
+
+//web accessibility
+//dom break point.
+//event listener.
+//right click and copy the style.
+
+
+//40-3 Console tab, preserve log, log levels, console api
+
+//${} can be use as quarrySelectorAll
+//console can be logged many ways in console.
+//ex:
+// console.info("normal info")
+//console.warn("Hello")
+//console.error("hello")
+
+//clear() to clear the console.
+
+//monitor
+//===================================
+//to monitor any function, work only in console.
+//monitor(updateProductNumber), when this function will be called, this monitor() function will give some reading.
+//unmonitor(theFunctionName), will stop monitoring.
+
+//event monitor(all)
+//const phonePlus = ${"#phone-plus"}
+//monitorEvent(phonePlus) , to monitor all event on that element.
+//monitorEvent(phonePlus, ["click, mouseout"]) , to monitor specific event.
+//unmonitorEvent(phonePlus), will stop monitoring.
+//in vs code, write in any function, console.count("write any thing or write the function name"), it shows in the browser console, how many times this function is being called.
+
+
+//search for to find more options about dev tools: chrome dev tools (console API reference)
+//copy("56",anything), copy any thing in the parenthesis
+//getEventListeners(phonePlus), to see how many event listener are their, in one element.
+
+
+//40-4 Sources tab, break point, call stack, event break
+
+//source tab: is more common tab for debugging.
+//============================================
+
+//connecting source tab with vs code.
+//=====================================
+//go to>
+//add folder to workspace> 
+//find the project file >
+//allow to connect.
+
+//snippets tab
+//=====================================
+//it save a formula which through, can be applied any website.
+
+//shortCut: ctrl + g to go any particular line. (works both vs and source tab)
+
+//ctrl + shift + O , to see every function in source code.
+//can be add break point in source tab and can be debug.
+//in source tab, press esc to open consol tab.
+
+//"pretty print" on top, to make readable coding of any website.
+
+
